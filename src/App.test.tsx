441bfa0a297306317106e703561fab0a1976002d
Blazhe Manev <blaze_manev@yahoo.com>
2025-08-10 18:48:14 +0200

    wrap App test with ActivityProvider and update assertion

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import { ActivityProvider } from "./context/ActivityContext";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders Activity Tracker header", () => {
+  render(
+    <ActivityProvider>
+      <App />
+    </ActivityProvider>
+  );
+
+  expect(
+    screen.getByRole("heading", { name: /activity tracker/i })
+  ).toBeInTheDocument();
 });
